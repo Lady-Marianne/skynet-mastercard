5fe6ac8fe272fd91ce33c1e3cadbd081a81e4107
Lady-Marianne <mariana.andrea.lois@gmail.com>
2024-10-01 13:50:25 -0300

    Casi finalizado

--- src/com/aluracursos/skynetmastercard/main/Main.java
+++ src/com/aluracursos/skynetmastercard/main/Main.java
@@ -1,7 +1,47 @@
 package com.aluracursos.skynetmastercard.main;
+import java.util.Collections;
+import java.util.Scanner;
+import com.aluracursos.skynetmastercard.models.Compra;
+import com.aluracursos.skynetmastercard.models.TarjetaDeCredito;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner lectura = new Scanner(System.in);
+        System.out.println("Escriba el límite de la tarjeta: ");
+        double limite = lectura.nextDouble();
+        TarjetaDeCredito tarjeta = new TarjetaDeCredito(limite);
 
+        int salir = 1;
+
+        while(salir != 0) {
+            System.out.println("Producto o servicio: ");
+            lectura.nextLine();
+            String descripcion = lectura.nextLine();
+
+            System.out.println("Valor: ");
+            double valor = Double.valueOf(lectura.next());
+
+            Compra compra = new Compra(descripcion, valor);
+            boolean compraRealizada = tarjeta.lanzarCompra(compra);
+
+            if (compraRealizada) {
+                System.out.println("¡Compra realizada!");
+                System.out.println("Escriba 0 para salir o 1 para continuar");
+                salir = lectura.nextInt();
+            } else {
+                System.out.println("¡Saldo insuficiente!");
+                salir = 0;
+            }
+        }
+        System.out.println("***********************");
+        System.out.println("COMPRAS REALIZADAS:\n");
+
+        Collections.sort(tarjeta.getListaDeCompras());
+
+        for (Compra compra : tarjeta.getListaDeCompras()) {
+            System.out.println(compra.getDescripcion() + " - " +compra.getValor());
+        }
+        System.out.println("\n***********************");
+        System.out.println("\nSaldo de la tarjeta: " +tarjeta.getSaldo());
     }
 }
---
+++ src/com/aluracursos/skynetmastercard/models/Compra.java
@@ -0,0 +1,29 @@
+package com.aluracursos.skynetmastercard.models;
+
+public class Compra implements Comparable<Compra>{
+    private String descripcion;
+    private double valor;
+
+    public Compra(String descripcion, double valor) {
+        this.descripcion = descripcion;
+        this.valor = valor;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    @Override
+    public String toString() {
+        return "Compra: " + descripcion + " Valor: " + valor;
+    }
+
+    @Override
+    public int compareTo(Compra otraCompra) {
+        return Double.valueOf(this.valor).compareTo(Double.valueOf(otraCompra.getValor()));
+    }
+}
